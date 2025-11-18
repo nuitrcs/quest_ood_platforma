92d0e125eb8d80f8d806eb3b196cef9600816557
hscarter <hscarter@gmail.com>
2025-11-18 14:37:10 -0600

    adding wait for output.log to have address local to after.sh

--- template/after.sh
+++ template/after.sh
@@ -4,6 +4,9 @@ echo "TIMING - Starting wait at: $(date)"
 if wait_until_port_used "localhost:${port}" 240; then
   echo "Discovered Platforma server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
+  until grep -q Address output.log; do
+    sleep 5
+    done
   export platformaurl=`cat output.log | grep Address`
 else
   echo "Timed out waiting for Platforma server to open port ${port}!"

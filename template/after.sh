a760a21e0153b1c1f00592e27c9f56769ec01138
hscarter <hscarter@gmail.com>
2025-11-18 13:58:10 -0600

    theoretically all changes to front remote server info to view

--- template/after.sh
+++ template/after.sh
@@ -1,2 +1,14 @@
-wait 120
-cat output.log | grep Address > file.txt
+# Wait for the Platforma server to start
+echo "Waiting for Platforma server to open port ${port}..."
+echo "TIMING - Starting wait at: $(date)"
+if wait_until_port_used "${host}:${port}" 180; then
+  echo "Discovered Platforma server listening on port ${port}!"
+  echo "TIMING - Wait ended at: $(date)"
+  export platformaurl=`cat output.log | grep Address`
+else
+  echo "Timed out waiting for Platforma server to open port ${port}!"
+  echo "TIMING - Wait ended at: $(date)"
+  pkill -P ${SCRIPT_PID}
+  clean_up 1
+fi
+

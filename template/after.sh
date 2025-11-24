c2448a321b96c351a5a3fc713432bc2e8eb868fc
hscarter <hscarter@gmail.com>
2025-11-24 15:06:49 -0600

    trying while loop instead

--- template/after.sh
+++ template/after.sh
@@ -4,9 +4,13 @@ echo "TIMING - Starting wait at: $(date)"
 if wait_until_port_used "localhost:${port}" 240; then
   echo "Discovered Platforma server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
-  until [ -s output.log ] & [ -n $"sed -n /Address/p output.log" ]; do
-    sleep 5
+  export PLATFORMAURL=`sed -n /Address/p output.log`
+  while [[ -z ${PLATFORMAURL} ]]; do
+	  export PLATFORMAURL=`sed -n /Address/p output.log`
   done
+ # until [ -s output.log ] & [ -n $"sed -n /Address/p output.log" ]; do
+ #   sleep 5
+ # done
   export PLATFORMAURL=`sed -n /Address/p output.log`
 else
   echo "Timed out waiting for Platforma server to open port ${port}!"

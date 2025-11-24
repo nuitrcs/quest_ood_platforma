384f141b99da6812483ac40e48d52e17725f7cd8
hscarter <hscarter@gmail.com>
2025-11-24 11:46:14 -0600

    added url def to before

--- template/after.sh
+++ template/after.sh
@@ -4,10 +4,10 @@ echo "TIMING - Starting wait at: $(date)"
 if wait_until_port_used "localhost:${port}" 240; then
   echo "Discovered Platforma server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
-  until { sed -n /Address/p output.log; i=$?; }; do
+  until [ -s output.log ] & [ -n $"sed -n /Address/p output.log" ]; do
     sleep 5
   done
-  export platformaurl=`cat output.log | grep Address`
+  export PLATFORMAURL=`sed -n /Address/p output.log`
 else
   echo "Timed out waiting for Platforma server to open port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
